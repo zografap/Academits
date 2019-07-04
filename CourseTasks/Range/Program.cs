d6e57de6eb487a1b5b819f2986ebad9edd09fd7c
zografap <zograf.anton@yandex.ru>
2019-07-04 07:52:50 +0700

    Добавлена Range (вторая часть)

--- CourseTasks/Range/Program.cs
+++ CourseTasks/Range/Program.cs
@@ -7,27 +7,68 @@ namespace Range
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите начало диапазона");
+            Console.WriteLine("Введите начало диапазона 1");
             double from = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите конец диапазона");
+            Console.WriteLine("Введите конец диапазона 1");
             double to = Convert.ToDouble(Console.ReadLine());
 
             Range range1 = new Range(from, to);
 
-            Console.WriteLine("Введите число X");
-            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите начало диапазона 2");
+            double from2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Длинна диапазона = " + range1.GetLength());
+            Console.WriteLine("Введите конец диапазона 2");
+            double to2 = Convert.ToDouble(Console.ReadLine());
 
-            if (range1.IsInside(x))
+            Range range2 = new Range(from2, to2);
+
+            Console.WriteLine("Длинна диапазона 1 = " + range1.GetLength());
+            Console.WriteLine("Длинна диапазона 2 = " + range2.GetLength());
+
+            double[] array = { from, to, from2, to2 };
+            Array.Sort(array);
+
+            if (Range.IntersectionIntervals(range1, range2))
             {
-                Console.WriteLine("Число принадлежит диапазону");
+                Console.WriteLine("Интервалы пересекаются");
+                Console.WriteLine("Результат пересечения интервалов:");
+                Console.WriteLine("от " + array[1] + " до " + array[2]);
+                Console.WriteLine("Результат объединения интервалов:");
+                Console.WriteLine("от " + array[0] + " до " + array[3]);
+            }
+            
+            else
+            {
+                Console.WriteLine("Интервалы не пересекаются");
+                Console.WriteLine("Результат объединения интервалов:");
+                Console.WriteLine("от " + array[0] + " до " + array[1] +
+                    " и " + "от " + array[2] + " до " + array[3]);
+
+            }
+            
+               
+            
+
+            double[] rezult = Range.DifferenceIntervals(range1, range2);
+            Console.WriteLine("Результат разности интервалов:");
+            if (rezult ==null)
+            {
+                Console.WriteLine("null");
             }
             else
             {
-                Console.WriteLine("Число не принадлежит диапазону");
+                Console.WriteLine("от " +rezult[0]+" до "+ rezult[1]);
+                if (rezult[2] != null)
+                {
+                    Console.WriteLine("и от " + rezult[2] + " до " + rezult[3]);
+                }
             }
+
+            
+
+
+            //Console.WriteLine("от " + rezult[0] + " до " + rezult[1]);
             Console.ReadKey();
         }
     }
--- CourseTasks/Range/Range.cs
+++ CourseTasks/Range/Range.cs
@@ -21,5 +21,71 @@
         {
             return (x >= From && x <= To);
         }
+
+        public static bool IntersectionIntervals(Range interval1, Range interval2)
+        {
+            return (interval1.From <= interval2.To && interval1.To >= interval2.From);
+        }
+
+        public static double[] DifferenceIntervals(Range interval1, Range interval2)
+        {
+            double[] rezult = new double[4];
+            if (IntersectionIntervals(interval1, interval2))
+            {
+                if (interval1.From == interval2.From && interval1.To == interval2.To)
+                {
+                    return null;
+                }
+                else if (interval1.From == interval2.From && interval1.To > interval2.To)
+                {
+                    rezult[0] = interval2.To;
+                    rezult[1] = interval1.To;
+                    return rezult;
+                }
+                else if (interval1.From == interval2.From && interval1.To < interval2.To)
+                {
+                    return null;
+                }
+                else if (interval1.From > interval2.From && interval1.To == interval2.To)
+                {
+                    return null;
+                }
+                else if (interval1.From < interval2.From && interval1.To == interval2.To)
+                {
+                    rezult[0] = interval1.From;
+                    rezult[1] = interval2.From;
+                    return rezult;
+                }
+                else if (interval1.From > interval2.From && interval1.To < interval2.To)
+                {
+                    return null;
+                }
+                else if (interval1.From < interval2.From && interval1.To > interval2.To)
+                {
+                    rezult[0] = interval1.From;
+                    rezult[1] = interval2.From;
+                    rezult[2] = interval2.To;
+                    rezult[3] = interval1.To;
+                    return rezult;
+                }
+                else if (interval1.From < interval2.From && interval1.To < interval2.To)
+                {
+                    rezult[0] = interval1.From;
+                    rezult[1] = interval2.From;
+                    rezult[2] = 0;
+                    rezult[3] = 0;
+                    return rezult;
+                }
+                else if (interval1.From > interval2.From && interval1.To > interval2.To)
+                {
+                    rezult[0] = interval1.From;
+                    rezult[1] = interval2.From;
+                    rezult[2] = 0;
+                    rezult[3] = 0;
+                    return rezult;
+                }
+            }
+            return null;
+        }
     }
 }
