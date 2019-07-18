dd60ffeff0aba479225d0047b07c2aa4a39456ce
zografap <zograf.anton@yandex.ru>
2019-07-18 11:26:13 +0700

    В конструктор Matrix добавлено ArgumentException

--- CourseTasks/Matrix/Matrix.cs
+++ CourseTasks/Matrix/Matrix.cs
@@ -1,4 +1,5 @@
-﻿using Vec = Vector.Vector;
+﻿using System;
+using Vec = Vector.Vector;
 namespace Matrix
 {
     class Matrix : Vec
@@ -7,7 +8,11 @@ namespace Matrix
         public Vec[] MatrixArray { get; set; }
 
         public Matrix(int n, int m)
-        {            
+        {
+            if (n <= 0 || m <= 0)
+            {
+                throw new ArgumentException("n и m должны быть > 0", nameof(n));
+            }
             Vec vector = new Vec(n);
             vector.N = n;
 
