d729c0fecbdbcce134ea36f4af32616bfe618122
zografap <zograf.anton@yandex.ru>
2019-08-12 20:10:46 +0700

    Выполнена задача List.

--- CourseTasks/List/Program.cs
+++ CourseTasks/List/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace List
 {
@@ -10,22 +6,55 @@ namespace List
     {
         static void Main(string[] args)
         {
-            SinglyLinkedList<string> linkedList = new SinglyLinkedList<string>();
-            
-            linkedList.InsertElementToBeginning("Светлана");
-            linkedList.InsertElementToBeginning("Василий");
-            linkedList.InsertElementToBeginning("Игорь");
-            linkedList.InsertElementToBeginning("Ольга");
-            linkedList.InsertElementToBeginning("Роман");
-            
-            Console.WriteLine(linkedList.ToString());
+            try
+            {
+                SinglyLinkedList<string> linkedList = new SinglyLinkedList<string>();
 
-            Console.WriteLine("Количество элементов: " + linkedList.GetSize());
-            Console.WriteLine("Получим первый элемент: " + linkedList.GetValueFirstElement().Data);
-            Console.WriteLine("Получим элемент с индексом 3: " + linkedList.GetValueByIndex(3).Data);
-            linkedList.ChangeElementByIndex(3,"Вика");
-            Console.WriteLine("Заменим элемент с индексом 3 на Вику, получим: " + linkedList.ToString());
-            Console.ReadKey();
+                linkedList.InsertElementToBeginning("Светлана");
+                linkedList.InsertElementToBeginning("Василий");
+                linkedList.InsertElementToBeginning("Игорь");
+                linkedList.InsertElementToBeginning("Ольга");
+                linkedList.InsertElementToBeginning("Роман");
+
+                Console.WriteLine(linkedList);
+
+                Console.WriteLine("Количество элементов: " + linkedList.GetSize());
+                Console.WriteLine("Получим первый элемент: " + linkedList.GetValueFirstElement());
+                Console.WriteLine("Получим элемент с индексом 3: " + linkedList.GetValueByIndex(3));
+
+                linkedList.ChangeElementByIndex(3, "Вика");
+                Console.WriteLine("Заменим элемент с индексом 3 на Вику, получим: " + linkedList);
+
+                linkedList.DeleteItemByIndex(3);
+                Console.WriteLine("Удалим элемент с индексом 3 получим: " + linkedList);
+
+                linkedList.InsertElementByIndex("Сергей", 3);
+                Console.WriteLine("Вставим элемент Сергей по индексу 3 получим: " + linkedList);
+
+                linkedList.DeleteNodeByValue("Роман");
+                Console.WriteLine("Удалим элемент Роман получим: " + linkedList);
+
+                linkedList.DeleteFirstItem();
+                Console.WriteLine("Удалим первый элемент получим: " + linkedList);
+
+                linkedList.Reverse();
+                Console.WriteLine("Развернем список получим: " + linkedList);
+
+                SinglyLinkedList<string> copylinkedList = linkedList.Сopy();
+                Console.WriteLine("Скопируем список получим: " + copylinkedList);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
--- CourseTasks/List/SinglyLinkedList.cs
+++ CourseTasks/List/SinglyLinkedList.cs
@@ -15,16 +15,16 @@ namespace List
             return Count;
         }
 
-        public ListItem<T> GetValueFirstElement()
+        public T GetValueFirstElement()
         {
-            return Head;
+            return Head.Data;
         }
 
-        public ListItem<T> GetValueByIndex(int index)
+        public T GetValueByIndex(int index)
         {
             if (index > Count)
             {
-                throw new ArgumentException("index выходит за границы списка");
+                throw new IndexOutOfRangeException("index выходит за границы списка");
             }
 
             int n = 0;
@@ -33,26 +33,130 @@ namespace List
             {
                 if (n == index)
                 {
-                    return p;
+                    return p.Data;
                 }
                 n++;
             }
-            return null;
+            return default(T);
         }
 
-        public ListItem<T> ChangeElementByIndex(int index, T data)
+        public T ChangeElementByIndex(int index, T data)
         {
-            ListItem<T> p = GetValueByIndex(index);
-            p.Data = data;
-            return null;
+            T oldValue = GetValueByIndex(index);
+            int n = 0;
+
+            for (ListItem<T> p = Head; p != null; p = p.Next)
+            {
+                if (n == index)
+                {
+                    p.Data = data;
+                }
+                n++;
+            }
+
+            return oldValue;
         }
 
-        public void InsertElementToBeginning(T Data)
+        public T DeleteItemByIndex(int index)
         {
-            ListItem<T> listItem = new ListItem<T>(Data);
+            T oldValue = GetValueByIndex(index);
+            int n = 0;
+
+            for (ListItem<T> p = Head, prev = null; p != null; prev = p, p = p.Next)
+            {
+                if (n == index)
+                {
+                    prev.Next = p.Next;
+                    Count--;
+                }
+                n++;
+            }
+
+            return oldValue;
+        }
+
+        public void InsertElementToBeginning(T data)
+        {
+            ListItem<T> listItem = new ListItem<T>(data);
             listItem.Next = Head;
             Head = listItem;
             Count++;
+        }
+
+        public void InsertElementByIndex(T data, int index)
+        {
+            ListItem<T> listItem = new ListItem<T>(data);
+            int n = 0;
+
+            for (ListItem<T> p = Head, prev = null; p != null; prev = p, p = p.Next)
+            {
+                if (n == index)
+                {
+                    prev.Next = listItem;
+                    listItem.Next = p;
+                    Count++;
+                }
+                n++;
+            }
+        }
+
+        public bool DeleteNodeByValue(T data)
+        {
+            for (ListItem<T> p = Head, prev = null; p != null; prev = p, p = p.Next)
+            {
+                if (p.Data.Equals(data))
+                {
+                    if (prev != null)
+                    {
+                        prev.Next = p.Next;
+                    }
+                    else
+                    {
+                        Head = Head.Next;
+                    }
+                    Count--;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public T DeleteFirstItem()
+        {
+            T oldValue = Head.Data;
+            Head = Head.Next;
+            Count--;
+            return oldValue;
+        }
+
+        public void Reverse()
+        {
+            ListItem<T> prev = null, current = Head, next = null;
+
+            while (current != null)
+            {
+                next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+            Head = prev;
+        }
+
+        public SinglyLinkedList<T> Сopy()
+        {
+            SinglyLinkedList<T> copyList = new SinglyLinkedList<T> { };
+
+            for (ListItem<T> p = Head, prev = null; p != null; prev = p, p = p.Next)
+            {
+                copyList.InsertElementToBeginning(p.Data);
+            }
+
+            copyList.Reverse();
+
+            return copyList;
         }
 
         public override string ToString()
